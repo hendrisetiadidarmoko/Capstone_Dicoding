4e3eea3c256ada32134ab5d42c7fccee928fc3f1
Navellia <ncent4lyfe@gmail.com>
2023-11-27 18:00:11 +0700

    batal

---
+++ Edugot_Capstone/resources/views/home.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+
+</head>
+
+<body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand">Navbar</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <main>
+        <section class="hero-element" id="hero">
+            <div class="container-fluid banner">
+                <div class="container text-center">
+                    <h1 class="display-4">Maggot Solusi untuk Pengelolaan Sampah Rumah Tangga </h1>
+                    <h3>Maggot, atau larva lalat Black Soldier Fly (BSF), merupakan salah satu solusi untuk pengelolaan
+                        sampah organik. Maggot memiliki kemampuan untuk mengurai sampah organik dengan cepat dan
+                        efisien.</h3>
+                    <a href="" class="btn btn-secondary btn-sm">Beli Sekarang</a>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="content-service container mt-3">
+                <h2 class="text-center mb-3">Layanan yang Kami Sediakan</h2>
+                <div class="container-service text-center">
+                    <div class="service rounded card ">
+                        <i class="mdi mdi-store"></i>
+                        <a href="">
+                            <h3>Pembelian</h3>
+                        </a>
+                        <p>Untuk memudahkan masyarakat dalam memperoleh maggot secara online</p>
+                    </div>
+                    <div class="service rounded card mx-2">
+                        <i class="mdi mdi-map-marker"></i>
+                        <a href="">
+                            <h3>Lokasi</h3>
+                        </a>
+                        <p>Memberikan informasi kepada masyarakat tentang lokasi peternak maggot di sekitar mereka.</p>
+                    </div>
+                    <div class="service rounded card">
+                        <i class="mdi mdi-store"></i>
+                        <a href="">
+                            <h3>Konsultasi</h3>
+                        </a>
+                        <p>Membantu pengguna dalam pengelolaan sampah rumah tangga dengan menggunakan maggot..</p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="descripsi-content">
+            <div class="container mt-5 ">
+                <div class="row gx-5 ">
+                    <div class="col ">
+                        <div class="box d-flex">
+                            <div class="container-img">
+                                <img src="../img/Maggot.png" alt="Your Image" class="image">
+                            </div>
+                            <div class="box-top"></div>
+                            <div class="box-bottom"></div>
+                        </div>
+                    </div>
+                    <div class="descripsi col">
+                        <h2>Lorem ipsum dolor sit amet Vel proin nunc eget at in amet.</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur. Volutpat felis varius venenatis morbi lectus
+                            vestibulum. Sem sit lectus sit turpis scelerisque sed pretium amet. Nunc feugiat orci sit in
+                            ut elementum.
+                            Nisi varius massa volutpat eu viverra amet. Arcu mattis fames ornare urna mauris eu. Sapien
+                            ut risus blandit duis adipiscing auctor tellus tincidunt. Ac sollicitudin mi sapien accumsan
+                            nunc id non nisl aenean. Integer nunc mi est tristique.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+
+
+    <script src="./index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
